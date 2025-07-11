e2ac9315b30e918099985fcec1ef4f8643317010
jelesho <jelesho100@gmail.com>
2025-07-10 23:09:49 -0400

    added undo

--- js/app.js
+++ js/app.js
@@ -19,9 +19,15 @@ const keyboardElement = document.querySelector('#keyboard')
 //display text of the button click on the keyboard
 const display = document.querySelector('.display')
 const enter = document.querySelector('.enter')
+const undo = document.querySelector('.undo')
+const userLetter = document.querySelectorAll('.user-letter')
+const wordOneRow = document.querySelectorAll('.row-one')
+const wordTwoRow = document.querySelectorAll('.row-two')
+const wordThreeRow = document.querySelectorAll('.row-three')
 
+//wordTwoRow[0].innerText = 'W';
 
-const numRows = 5; 
+const numRows = 5;
 const columnRows = 3;
 const wordBank = ['RIGHT']; //['RIGHT', 'STARS', 'MONEY', 'GRACE', 'SHOOT']
 const userWordBank = [];
@@ -31,9 +37,14 @@ const userWordBank = [];
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let guesses = 3;
-let userInput='';
+let guess = 3;
+let wordOne = [];
+let wordTwo = [];
+let wordThree = [];
 let randomWord = '';
+let userInput = '';
+let currentUserRow = 1;
+
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -41,43 +52,108 @@ let randomWord = '';
 
 
 /*-------------------------------- Functions --------------------------------*/
-                        //logic to pick random word from the array
+//logic to pick random word from the array
 function randWordGenerator() {
-     let randomIndex = Math.floor(Math.random() * wordBank.length);
-     randomWord = wordBank[randomIndex];
+    let randomIndex = Math.floor(Math.random() * wordBank.length);
+    randomWord = wordBank[randomIndex];
     //console.dir(randomWord + ' is console log');
     return randomWord;
+}
+function displayCorrectWord() {
+    display.innerText = randWordGenerator();
+    return '\nWord was: ' + randWordGenerator();
 }
 
 
 /*----------------------------- Event Listeners -----------------------------*/
 //makes button clickable and limits word to 5letters
 keyboardElement.addEventListener('click', (event) => {
-    if(event.target.classList.contains('letter') && userInput.length<5){ 
-        userInput+=event.target.innerText;
-        console.log(userInput);
-        display.innerText = userInput;
-    }
-})
-//logic to enter the word and have user enter next word
-enter.addEventListener('click', (event) => {
-    if(userInput.length===5){
-        display.innerText = userInput;
-        
-        console.log('entered')
-       
-        //win lose logic
-        if(randomWord === userInput){
-            console.log("You Win!")
-                display.innerText = "Congrats! You win!";
+    if (event.target.classList.contains('letter') && wordOne.length < 5) {
+        wordOne.push(event.target.innerText);
+        console.log(wordOne);
+        //loop through div class 
+        //dynamically set the proper array for what row the user is on
+        console.log(currentUserRow)
+        let currentArray, currentWordRow;
+        switch (currentUserRow) {
+            case 1: { 
+                currentArray = wordOne;
+                currentWordRow = wordOneRow; 
+                break
+            }
+            case 2: { 
+                currentArray = wordTwo;
+                currentWordRow = wordTwoRow;
+                break 
+            }
+            case 3: { 
+                currentArray = wordThree;
+                currentWordRow = wordThreeRow;
+                break 
+            }
+        }
+        for (let i = 0; i < currentArray.length; i++) {
+            currentWordRow[i].innerText = currentArray[i];
 
         }
-        userInput = '';
+        userInput = currentArray.join(''); //array method that takes an array on converts it to a single string
+        //display.innerText = userInput;
     }
-    
 })
 
+
+
+//logic to enter the word and have user enter next word 
+enter.addEventListener('click', (event) => {
+    if (guess === 1) {
+        console.log("Player has lost")
+        // enter.removeEventListener('click', event)
+        return display.innerText = displayCorrectWord();
+    }
+    //loop to track how many guesses player has
+    // if(guess >= 2){ //
+    if (userInput.length === 5) {
+        // display.innerText = userInput;
+
+        console.log('entered')
+        guess--;
+        //win logic
+        if (randomWord === userInput) {
+            console.log("You Win!")
+            display.innerText = "Congrats! You win!";
+        }
+
+        userInput = '';
+    }
+    //increment the row the user is on
+    switch (guess) {
+        //fall through
+        case 3:
+        case 2: 
+            currentUserRow+=1;
+            break;
+        
+    }
+    //lose logic. stops user from entering once guess. hits 0?
+    // }
+    // else{
+    // console.log("Player has lost")
+    // enter.removeEventListener('click', event)
+
+    // return display.innerText = displayCorrectWord();
+
+    // }
+
+})
+//user can backspace
+undo.addEventListener('click', (event) => {
+    userInput = userInput.slice(0, -1)
+    display.innerText = userInput;
+})
+
+
 randWordGenerator();
+
 
 
 
